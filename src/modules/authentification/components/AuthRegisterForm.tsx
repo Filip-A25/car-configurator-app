82367d63aaadf4f513a7e6ec1d63560187a016d9
Filip-A25 <filip.agatic25@gmail.com>
2024-06-03 12:40:12 +0200

    Add sign in with google to register and login, add login form and sing in via email and password, add styling for register and login

--- src/modules/authentification/components/AuthRegisterForm.tsx
+++ src/modules/authentification/components/AuthRegisterForm.tsx
@@ -9,21 +9,33 @@ import {
   passwordMaxLength,
   passwordRegexp,
 } from "../const/userInputRequirements";
-import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
-import { auth } from "../../firebase/firebase";
-import { useNavigate } from "react-router-dom";
+import {
+  createUserWithEmailAndPassword,
+  updateProfile,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { auth, provider } from "../../firebase/firebase";
+import { useNavigate, Link } from "react-router-dom";
 import InputField from "./inputs/InputField";
+import { formOpenState } from "../state/formState";
+import googleLogoImg from "../assets/google-logo.png";
+
+interface UserCred extends User {
+  password: string;
+}
 
 export default function AuthRegisterForm() {
   const navigate = useNavigate();
 
-  const form = useForm<User>();
+  const form = useForm<UserCred>();
   const [userData, setUserData] = useRecoilState(userState);
   const [isLoggedIn, setIsLoggedIn] = useRecoilState(loggedState);
+  const [isFormOpen, setIsFormOpen] = useRecoilState(formOpenState);
 
-  const onSubmit = (data: User) => {
+  const onSubmit = (data: UserCred) => {
     createUserWithEmailAndPassword(auth, data.email, data.password)
-      .then(async (userCredential) => {
+      .then(async () => {
         if (!userCredential || !auth.currentUser) {
           throw new Error("User data could not be found.");
         }
@@ -35,7 +47,6 @@ export default function AuthRegisterForm() {
         setUserData({
           name: data.name,
           email: data.email,
-          password: data.password,
         });
         setIsLoggedIn(true);
         navigate("/");
@@ -45,71 +56,98 @@ export default function AuthRegisterForm() {
       });
   };
 
+  const handleGoogleSignUp = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const userCredential = GoogleAuthProvider.credentialFromResult(result);
+        const token = userCredential?.accessToken;
+        const user = result.user;
+
+        if (!user.displayName || !user.email || !token)
+          throw new Error("User data could not be found.");
+
+        setUserData({
+          name: user.displayName,
+          email: user.email,
+          googleAccessToken: token,
+        });
+
+        setIsLoggedIn(true);
+        navigate("/");
+      })
+      .catch((err) => {
+        throw new Error(err.message);
+      });
+  };
+
   return (
-    <FormProvider {...form}>
-      <form
-        id="register-form"
-        onSubmit={form.handleSubmit(onSubmit)}
-        className="flex flex-col w-screen sm:w-[500px] bg-basic-white text-text-default-gray px-10 py-8 sm:shadow-md"
-      >
-        <h2 className="form-size-header font-bold">Sign Up</h2>
-        <span className="text-sm">Don't have an account? Create one now!</span>
-        <section className="flex flex-col justify-between h-[320px] my-5 pb-5 sm:pb-10">
-          <label>Nickname</label>
-          <InputField
-            name="name"
-            placeholder="Enter a nickname..."
-            type="text"
-            validation={{
-              required: true,
-              minLength: {
-                value: usernameMinLength,
-                message: `Nickname must be atleast ${usernameMinLength} characters long.`,
-              },
-              maxLength: {
-                value: usernameMaxLength,
-                message: `Nickname cannot be longer than ${usernameMaxLength} characters.`,
-              },
-            }}
-          />
-          <label>E-mail address</label>
-          <InputField
-            name="email"
-            placeholder="Enter your email..."
-            type="email"
-            validation={{ required: true }}
-          />
-          <label>Password</label>
-          <InputField
-            name="password"
-            placeholder="Enter a password..."
-            type="password"
-            validation={{
-              required: true,
-              minLength: {
-                value: passwordMinLength,
-                message: `Password must be atleast ${passwordMinLength} characters long.`,
-              },
-              maxLength: {
-                value: passwordMaxLength,
-                message: `Password cannot be longer than ${passwordMaxLength} characters.`,
-              },
-              pattern: {
-                value: passwordRegexp,
-                message:
-                  "Password must contain atleast one uppercase letter, one lowercase letter and a number or a special character.",
-              },
-            }}
-          />
-        </section>
-        <button
-          form="register-form"
-          type="submit"
-          className="bg-button-purple h-[48px] sm:h-[44px] w-full sm:w-[117px] text-basic-white mx-auto hover:brightness-[110%] transition-all delay-100 ease-in-out"
-        >
-          Sign Up
-        </button>
-      </form>
-    </FormProvider>
+    <section className="relative flex flex-col w-screen sm:w-[500px] h-full sm:h-[500px] bg-basic-white text-text-default-gray px-10 py-8 shadow-md">
+      <h2 className="text-[2.5rem] font-bold">Sign Up</h2>
+      <span className="text-sm">
+        Already have an account?{" "}
+        <Link to="/auth/log-in" className="text-text-purple">
+          Sign in.
+        </Link>
+      </span>
+
+      {!isFormOpen ? (
+        <div className="mt-8 flex flex-col items-center justify-evenly h-[30%] sm:h-[40%]">
+          <button
+            className="bg-button-purple right-0 left-0 h-[60px] sm:h-[52px] w-full text-basic-white mx-auto hover:brightness-[110%] transition-all delay-100 ease-in-out"
+            onClick={() => setIsFormOpen(true)}
+          >
+            Sign up with e-mail
+          </button>
+          <button
+            className="flex justify-center items-center right-0 left-0 h-[60px] sm:h-[52px] w-full text-button-purple border-[1px] mx-auto hover:brightness-[110%] transition-all delay-100 ease-in-out"
+            onClick={handleGoogleSignUp}
+          >
+            <img
+              src={googleLogoImg}
+              alt="Google logo"
+              className="w-[20px] h-[20px] mr-2"
+            />
+            <span>Sign up with a Google account</span>
+          </button>
+        </div>
+      ) : (
+        <FormProvider {...form}>
+          <form id="register-form" onSubmit={form.handleSubmit(onSubmit)}>
+            <section className="flex flex-col justify-between h-[280px] my-5 pb-5 sm:pb-10">
+              <label>Nickname</label>
+              <input
+                className="h-[48px] sm:h-[32px] rounded-input-radius p-input-padding outline-none border-[1px] border-input-border-gray mb-2"
+                {...form.register("name", {
+                  required: true,
+                  minLength: {
+                    value: usernameMinLength,
+                    message: `Nickname must be atleast ${usernameMinLength} characters long.`,
+                  },
+                  maxLength: {
+                    value: usernameMaxLength,
+                    message: `Nickname cannot be longer than ${usernameMaxLength} characters.`,
+                  },
+                })}
+                placeholder="Enter a nickname..."
+              />
+              <label>E-mail address</label>
+              <EmailInput email="email" />
+              <label>Password</label>
+              <PasswordInput
+                password="password"
+                placeholder="Enter a password..."
+              />
+            </section>
+            <button
+              form="register-form"
+              type="submit"
+              className="absolute bg-button-purple bottom-[50px] right-0 left-0 h-[48px] sm:h-[44px] w-[85%] text-basic-white mx-auto hover:brightness-[110%] transition-all delay-100 ease-in-out"
+            >
+              Sign Up
+            </button>
+          </form>
+        </FormProvider>
+      )}
+    </section>
   );
 }
