e040e1d9bf252b8fcfe871420676b1121dbd1aed
Filip-A25 <filip.agatic25@gmail.com>
2024-06-05 13:43:48 +0200

    Add guard navigation for when user is logged in

--- src/modules/authentification/Authentication.tsx
+++ src/modules/authentification/Authentication.tsx
@@ -1,8 +1,23 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
 import AuthLoginForm from "./components/AuthLoginForm";
 import AuthRegisterForm from "./components/AuthRegisterForm";
+import { useEffect } from "react";
+import { useRecoilValue } from "recoil";
+import { pathSelector } from "../global/state/navigationState";
 
 export default function Authentication() {
+  const navigate = useNavigate();
+
+  const filteredRoutes = useRecoilValue(pathSelector);
+  const filteredPaths = filteredRoutes.map((route) => {
+    return route.path;
+  });
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    if (!filteredPaths.includes(pathname)) navigate("/");
+  }, []);
+
   return (
     <div className="flex justify-center sm:pt-10">
       <Routes>
