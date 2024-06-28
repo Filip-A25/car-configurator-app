f959c4a28102c4a7d1b003255fc53dbc0ba03d23
Filip-A25 <filip.agatic25@gmail.com>
2024-06-28 12:48:17 +0200

    Styling changes

--- src/modules/configurator/components/Configurations.tsx
+++ src/modules/configurator/components/Configurations.tsx
@@ -4,16 +4,19 @@ import { useRecoilValue } from "recoil";
 import { userConfigurationsState } from "../state";
 import { ConfigurationsSaved } from "./ConfigurationsSaved";
 
-export default function Configurations() {
+export function Configurations() {
   const userConfigurations = useRecoilValue(userConfigurationsState);
 
   return (
-    <section className="text-center md:text-left px-10 sm:px-20 md:px-20 lg:px-36 3xl:px-52 py-16 3xl:py-24 flex flex-col">
+    <section className="text-center md:text-left px-10 sm:px-20 md:px-20 lg:px-20 2xl:px-48 3xl:px-52 py-16 3xl:py-24 flex flex-col">
       <header className="flex flex-col sm:flex-row justify-between pb-10 3xl:pb-20">
-        <h2 className="text-lg sm:text-sm 3xl:text-2xl py-5">
+        <h2 className="text-lg sm:text-sm lg:text-xl 3xl:text-2xl sm:py-3 md:py-4 3xl:py-5">
           View saved configurations
         </h2>
-        <ConfigureLinkButton path="/configure-a-car" title="Configure a car" />
+        <ConfigureLinkButton
+          path="/home/configure-a-car/car-select"
+          title="Configure a car"
+        />
       </header>
       {!Boolean(userConfigurations.length) ? (
         <ConfigurationsEmpty />
