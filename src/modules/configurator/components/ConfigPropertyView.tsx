b059e2388ac753ca0763f3348c7a3455259fc6ec
Filip-A25 <filip.agatic25@gmail.com>
2024-07-08 23:20:44 +0200

    Minor logic fix

--- src/modules/configurator/components/ConfigPropertyView.tsx
+++ src/modules/configurator/components/ConfigPropertyView.tsx
@@ -1,12 +1,12 @@
 import { ConfigProperty } from "./ConfigProperty";
 import { CarProperty } from "../types";
 
-export function ConfigPropertyView({ price, ...props }: CarProperty) {
+export function ConfigPropertyView({ ...props }: CarProperty) {
   return (
     <div className="flex items-center justify-between cursor-text">
-      <ConfigProperty price={price} disabled {...props} />
+      <ConfigProperty disabled {...props} />
       <span className="text-md sm:text-xl text-property-name-grey">
-        {price} €
+        {props.price} €
       </span>
     </div>
   );
