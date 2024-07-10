fdd1a882894374f8026a64354de3fb67a490833b
Filip-A25 <filip.agatic25@gmail.com>
2024-07-10 16:34:51 +0200

    Minor styling changes

--- src/modules/configurator/components/ConfigEdit.tsx
+++ src/modules/configurator/components/ConfigEdit.tsx
@@ -15,7 +15,7 @@ export function ConfigEdit({ isDropdownOpen, activeDropdownName }: Props) {
       <section className="relative flex max-sm:flex-col min-h-[calc(100%-50px)] md:min-h-[calc(100%-60px)] lg:min-h-[calc(100%-70px)]">
         <div
           className={clsx(
-            "md:py-20 max-sm:relative flex sm:items-center max-sm:h-full w-full sm:w-[calc(100%-350px)] md:w-[calc(100%-425px)] lg:w-[calc(100%-475px)] 3xl:w-[calc(100%-565px)]",
+            "sm:py-20 max-sm:relative flex sm:items-center max-sm:h-full w-full sm:w-[calc(100%-350px)] md:w-[calc(100%-425px)] lg:w-[calc(100%-475px)] 3xl:w-[calc(100%-565px)]",
             activeDropdownName === "interior_variants"
               ? "max-xs:py-28 xs:py-40"
               : "max-xs:py-20 xs:py-32",
