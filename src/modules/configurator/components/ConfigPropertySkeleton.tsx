d3f094ee907c4aa7a2b8c2f088ebd70966e20333
Filip-A25 <filip.agatic25@gmail.com>
2024-07-12 13:05:12 +0200

    Add ConfigPropertySkeleton

---
+++ src/modules/configurator/components/ConfigPropertySkeleton.tsx
@@ -0,0 +1,8 @@
+export function ConfigPropertySkeleton() {
+  return (
+    <div className="flex items-center py-2 xs:py-4 sm:py-5">
+      <div className="w-12 2xl:w-14 h-12 2xl:h-14 bg-light-gray-background-color rounded-[50%] animate-pulse" />
+      <div className="mx-auto px-44 sm:px-24 md:px-32 h-4 bg-light-gray-background-color animate-pulse rounded-md" />
+    </div>
+  );
+}
