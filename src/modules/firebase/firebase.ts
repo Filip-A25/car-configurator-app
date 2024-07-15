497e71519dea3970c87ad2bcb29d6ee15e57cb70
Filip-A25 <filip.agatic25@gmail.com>
2024-07-15 15:49:41 +0200

    Add env variables in to firebase config

--- src/modules/firebase/firebase.ts
+++ src/modules/firebase/firebase.ts
@@ -4,12 +4,12 @@ import {GoogleAuthProvider, getAuth} from "firebase/auth";
 import {getStorage} from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyABt0HYROhhog4AMZWxhSCCUJdNtcMcPdU",
-  authDomain: "car-configuration-481af.firebaseapp.com",
-  projectId: "car-configuration-481af",
-  storageBucket: "car-configuration-481af.appspot.com",
-  messagingSenderId: "642741426747",
-  appId: "1:642741426747:web:d994a036afc620a04fda1e"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
