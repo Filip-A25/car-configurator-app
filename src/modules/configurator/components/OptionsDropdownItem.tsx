eb8edf97d9a382c306608a4c8c42b85ca76c8001
Filip-A25 <filip.agatic25@gmail.com>
2024-07-15 12:50:10 +0200

    Minor styling change

--- src/modules/configurator/components/OptionsDropdownItem.tsx
+++ src/modules/configurator/components/OptionsDropdownItem.tsx
@@ -10,7 +10,7 @@ export function OptionsDropdownItem({ title, ...props }: Props) {
       <button
         className={`${
           title === "Delete" ? "text-text-red" : "text-button-purple"
-        } flex w-full 3xl:text-2xl items-center max-md:py-3 max-md:pl-4 max-md:pr-10 lg:py-4 2xl:py-5 3xl:py-6 lg:pl-4 lg:pr-12 2xl:pl-8 2xl:pr-20 whitespace-nowrap !z-30`}
+        } flex w-full 3xl:text-2xl items-center max-md:py-3 max-md:pl-4 max-md:pr-10 md:py-3 md:pr-8 lg:py-4 2xl:py-5 3xl:py-6 lg:pl-4 lg:pr-12 2xl:pl-8 2xl:pr-20 whitespace-nowrap`}
         {...props}
       >
         {title}
