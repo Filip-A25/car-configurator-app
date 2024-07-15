dd54f4f7206bd9fed288c564a2c18879e9f75fcf
Filip-A25 <filip.agatic25@gmail.com>
2024-07-15 15:18:54 +0200

    Minor merge issue fix

--- src/modules/configurator/components/ConfigNavbar.tsx
+++ src/modules/configurator/components/ConfigNavbar.tsx
@@ -25,7 +25,7 @@ export function ConfigNavbar({
   return (
     <div className="h-[50px] sm:h-[70px] bg-light-gray-element-color border-b border-input-border-gray flex justify-between items-center px-4 sm:px-10 2xl:px-12">
       <section className="flex justify-between text-lg sm:text-xl lg:text-2xl 3xl:text-3xl text-text-default-gray">
-        <ReturnButton path={returnPath} />
+        <ReturnButton onClick={returnOnClick} returnPath={returnPath} />
         <h3 className="max-sm:hidden text-muted-grey font-optician-sans px-2">
           {productionYear}
         </h3>
